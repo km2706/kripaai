70feca7312abe15dce0c66911f13d2ae9b2f68a5
km2706 <mahidakripa9974@gmail.com>
2026-01-10 07:02:35 +0000

    dmo2

--- day2.py/p4.py
+++ day2.py/p4.py
@@ -5,3 +5,30 @@ Version:
 Release date:
 """
 #sequential dataype
+
+"""
+    1.List-[]
+    -mutable
+    -Any datatype is supported
+
+    2.tuple-()
+    -immutable
+
+    3.set-{}
+    -stores ony unique value
+
+    4.dictonary
+    -mutable
+    -{key:value}
+"""
+nums=[1,2,3]
+cart=["phone","book",10,false,None,{1:"a"},nums]
+
+print(cart)
+
+cart[0] = "pc"
+
+print(cart)
+print(type(cart))
+
+marks =
